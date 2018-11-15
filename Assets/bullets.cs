672caa1d06ad58bdc550e5f8a9d816aec8e1a75b
dairong <ambera23325a@gmail.com>
2018-11-15 16:08:17 +0800

    first commit

---
+++ Assets/bullets.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullets : MonoBehaviour {
+	//public Quaternion targetRotation;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("yoshi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((this.transform.position - target.transform.position).magnitude < 31.11f) {
+			Destroy (this.gameObject);
+		} else {
+			this.transform.position = Vector3.MoveTowards (transform.position,target.transform.position,10.0f);
+			//this.transform.rotation = quate;
+			Vector3 rot = new Vector3((target.transform.position - transform.position).x,(target.transform.position - transform.position).y-90.0f,(target.transform.position - transform.position).z);
+			Quaternion rotation = Quaternion.LookRotation(rot);
+			this.transform.rotation = rotation;
+		}
+	}
+}
---
+++ Assets/shot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shot : MonoBehaviour {
+	public GameObject bullet;
+	private float timer;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= 2.0f) {
+			//if()
+			GameObject bullets = Instantiate(bullet,transform.position,Quaternion.identity) as GameObject;
+			bullets.AddComponent<bullets> ();
+			timer = 0;
+		}
+		Vector3 rot = new Vector3 ((target.transform.position - transform.position).x,(target.transform.position - transform.position).y,180);
+		transform.rotation = Quaternion.LookRotation(rot);
+	}
+}
